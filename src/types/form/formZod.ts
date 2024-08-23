f9ca0d1f17b4c8e8ee459bf31b15a525dbfcdf59
Jordan Higuera Higuera <jordandark123@gmail.com>
2024-08-23 10:28:52 -0700

    feat: changes

--- src/types/form/formZod.ts
+++ src/types/form/formZod.ts
@@ -1,9 +1,11 @@
 import { z } from "zod";
 
 export const formSchema = z.object({
-  name: z.string().min(3, "Name must be at least 3 characters long").max(50, "Name must be at most 50 characters long"),
+  // Regex for only strings not numbers
+
+  name: z.string().regex(/^(?!.*\d+$).*$/, "Numbers are not allowed").min(3, "Name must be at least 3 characters long").max(50, "Name must be at most 50 characters long"),
   email: z.string().email(),
-  age: z.number().int().positive("Age must be a positive number"),
+  age: z.number().int().positive("Age must be a positive number").min(1).max(99, "Age must be at most 99").transform((val) => Number(val)),
   phoneNumber: z.string().regex(/^\d{10}$/, "Phone number must be 10 digits long"),
   occupation: z.string().min(3, "Occupation must be at least 3 characters long").max(50, "Occupation must be at most 50 characters long"),
   hobby: z.string().min(3, "Hobby must be at least 3 characters long").max(50, "Hobby must be at most 50 characters long"),
